6285c507047fb55b9c1fe45bed4a1a41f55add1c
Soham Sawant <sohamsawant2311@gmail.com>
2025-08-11 23:51:41 +0530

    read the comments to be done tom

---
+++ main.ino
@@ -0,0 +1,7 @@
+// what should this contain 
+// it should loop in the while the bot is traversing 
+//read senosors
+//update maze map
+//decide the next move accroding to the weights
+//drive drive
+//ha ha repeat
