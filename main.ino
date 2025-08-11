6001590a63028d1569f431421618d02940a78594
Soham Sawant <sohamsawant2311@gmail.com>
2025-08-12 00:05:16 +0530

    mae this confirm 1st

--- main.ino
+++ main.ino
@@ -1,7 +1,24 @@
 // what should this contain 
-// it should loop in the while the bot is traversing 
+// it should loop while the bot is traversing 
 //read senosors
 //update maze map
 //decide the next move accroding to the weights
 //drive drive
 //ha ha repeat
+#include "encoder.hpp"
+#include "sensors.hpp"
+#include "motors.hpp"
+#include "pid.hpp"
+#include "navigation.hpp"
+#include "maze.hpp"
+#include "utils.hpp"
+
+
+// making global objects to avoid iteration in setup or loop 
+
+Encoder encoderLeft, encoderRight;// measuring the distance travelled by motors and for pid correction
+Sensors sensors;//hehe again detect
+Motors motors;//motors pwm
+PIDController pid;//pidpid
+Navigation navigation;//it takes the decision and bots orintation
+Maze maze;//holds the maze 
